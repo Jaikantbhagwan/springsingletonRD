f3a060271999d442208f92f57305a852bf70e88e
Jaikant S <jaikant_singh@apple.com>
2017-11-15 17:23:56 +0530

    Added some more scenario

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,24 @@ public class Main {
 
     public static void main(String[] args) {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
-        Scope scope = (Scope)ctx.getBean("scopeTest");
-        Scope scopeDuplicate = (Scope)ctx.getBean("scopeTestDuplicate");
-        System.out.println(scope == scopeDuplicate);
-        System.out.println(scope + "::" + scopeDuplicate);
+        // Get same
+        Scope scope1 = (Scope)ctx.getBean("scopeTest");
+        Scope scope2 = (Scope)ctx.getBean("scopeTest");
+
+        System.out.println(scope1 == scope2);
+
+        Scope scopeDuplicate1 = (Scope)ctx.getBean("scopeTestDuplicate");
+
+        System.out.println(scope1 == scopeDuplicate1);
 
         ApplicationContext ctx1 = new ClassPathXmlApplicationContext("beans.xml");
-        Scope scope1 = (Scope)ctx1.getBean("scopeTest");
 
-        System.out.println(scope == scope1);
+        Scope scope3 = (Scope)ctx1.getBean("scopeTest");
+        Scope scopeDuplicate2 = (Scope)ctx1.getBean("scopeTestDuplicate");
+
+        System.out.println(scope1 == scope3);
+
+        System.out.println(scopeDuplicate1 == scopeDuplicate2);
 
     }
 
